663f619b13caf17c6dbb0f10aeaf253f693f41f4
salmanah <salardahm@gmail.com>
2016-09-30 15:40:40 +0200

    Finished PascalDecl and sub-classes

--- parser/FuncDecl.java
+++ parser/FuncDecl.java
@@ -8,8 +8,36 @@ import static scanner.TokenKind.*;
 class FuncDecl extends ProcDecl {
     ParamDeclList pdl;
     Block b;
+    Type type;
 
-    FuncDecl(String id) {
+    FuncDecl(String id, int lNum) {
+        super(id, lNum);
+    }
+
+    @Override public String identify() {
+        retunr "<Func-decl> "+ name + "on line " + lineNum
+    }
+
+    static FuncDecl parse(Scanner s) {
+        enterParser("Func-decl");
+        s.skip(functionToken);
+        s.test(nameToken);
+
+        FuncDecl fd = new FuncDecl(s.curToken.id, s.curLineNum());
+
+        if (s.curToken.id != colonToken) {
+            fd.pdl = ParamDeclList.parse(s);
+        }
+
+        s.skip(colonToken);
+        fd.type = Type.parse(s);
+        s.skip(semicolonToken);
+
+        fd.b = Block.parse(s);
+        s.skip(semicolonToken);
+
+        leaveParser("Func-decl");
+        return fd;
 
     }
 }
