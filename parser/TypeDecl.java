28e550dd61b327e3a6736b8563c5209d5f5734fc
salmanah <salardahm@gmail.com>
2016-10-10 15:11:45 +0200

    Fixed Typedecl

--- parser/TypeDecl.java
+++ parser/TypeDecl.java
@@ -6,5 +6,29 @@ import java.util.ArrayList;
 import static scanner.TokenKind.*;
 
 class TypeDecl extends PascalDecl{
-    
+
+    TypeDecl(String id, int lNum){
+        super(id,lNum); 
+    }
+    @Override public String identify() {
+        return "<Param-decl> " + name + "on line " + lineNum;
+    }
+
+    static TypeDecl(Scanner s){
+
+    }
+
+    void checkWhetherAssignable(PascalSyntax where){
+
+    }
+    void checkWhetherFunction(PascalSyntax where){
+
+    }
+    void checkWhetherProcedure(PascalSyntax where){
+
+    }
+    void checkWhetherValue(PascalSyntax where){
+
+    }
+
 }
